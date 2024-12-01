e552a1ec23ce06d717c1e28df559e44966364f8e
anus26 <anusraza268gmail.com>
2024-12-01 10:28:26 -0800

    encrypt password

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ dotenv.config();
 import express from "express";
 import connectDB from "./src/db/index.js"
 import cors  from "cors"
+import cookieParser  from 'cookie-parser'
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
+import userModels from "./src/models/user.models.js"; // Import user model
 
 
 
 const app = express();
 app.use(cors())
 app.use(express.json());
-
+app.use(cookieParser())
+const JWT_TOkEN_SECRET=process.env.JWT_TOkEN_SECRET
 
 
 app.get("/", (req, res) => {
@@ -19,7 +22,31 @@ app.get("/", (req, res) => {
 });
 console.log("Mongo URI:", process.env.MONGO_URI);
 
- 
+const encryptpassword="$2b$10$gqZs1GSEe9XHl64sYPOiwOvIO7YDimLPtwSf6lkDx1M4PTCoE..CK"
+
+app.post("/encryptpassword",(req,res)=>{
+    const {password}=req.body
+    bcrypt.hash(password, 10, function(err, hash) {
+        if(err) return res.status(402).json({message:"password error"})
+            res.json({password:hash})
+        // Store hash in your password DB.
+    });
+})
+
+app.post("/checkpassword",(req,res)=>{
+    const {password}=req.body
+    bcrypt.compare(password, encryptpassword, function(err, result) {
+        if(err) return res.status(402).json({message:"occur error"})
+        // result == true
+    if(result) return res.json({
+        message:"password is correct"
+    })
+    res.status(404).json({message:"incorrect password"})
+    });
+})
+
+
+
 
 // Database Connection and Server Start
 connectDB()
